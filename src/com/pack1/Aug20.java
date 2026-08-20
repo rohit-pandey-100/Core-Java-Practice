4f1ab25b85c23099020d08cf7516e6f50990db0b
Rohit Pandey <krohit30965@gmail.com>
2026-08-21 00:02:14 +0530

    On the super() or thsi() and Super and this keyword code practice

---
+++ src/com/pack1/Aug20.java
@@ -0,0 +1,9 @@
+package com.pack1;
+
+public class Aug20
+{
+	Aug20(int x)
+	{
+		
+	}
+}
---
+++ src/com/pack1/SecondAug20.java
@@ -0,0 +1,14 @@
+package com.pack1;
+
+public class SecondAug20 extends Aug20 
+{
+	SecondAug20(int x)
+	{
+		super(50);
+	}
+	public static void main(String[] args) 
+	{
+		
+		SecondAug20 obj=new SecondAug20(50);
+	}
+}
